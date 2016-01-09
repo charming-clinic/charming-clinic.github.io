2b16adc905f02382ae893cf2075cccf010b9354f
bclow <bclow@yahoo-inc.com>
2016-01-10 02:26:05 +0800

    use generic browser detection javascript

--- script/browser.js
+++ script/browser.js
@@ -1,10 +1,15 @@
      function DetectTheThing()
     {
        var uagent = navigator.userAgent.toLowerCase();
+       var filename = window.location.href.split('/').pop()
+       var newfilename = "m" + filename;
+       //alert(filename + " -> " + newfilename);
+
+
        if (uagent.search("iphone") > -1 || uagent.search("ipad") > -1 
        || uagent.search("android") > -1 || uagent.search("blackberry") > -1
        || uagent.search("webos") > -1) {
-          window.location.href ="mindex.htm";
+          window.location.href = newfilename;
        }        
     }
 
